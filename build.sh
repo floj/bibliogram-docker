a07443d9351648703f2fd483e0230e8e544adda6
floj <floj@users.noreply.github.com>
2022-04-27 12:10:53 +0200

    refine build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-docker build -t floj/bibliogram .
+ead() {
+  printf "%s\n" "$*"
+  "$@"
+}
+
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "$scriptDir"
+
+imageName=ghcr.io/floj/bibliogram
+
+declare -a dockerOpts=(-t "$imageName")
+
+if [[ -n "${FORCE_BUILD:-}" ]]; then
+  dockerOpts+=("--no-cache")
+fi
+
+ead docker build "${dockerOpts[@]}" "$scriptDir"
+if [[ -n "${PUSH:-}" ]]; then
+  docker push "$imageName"
+fi
